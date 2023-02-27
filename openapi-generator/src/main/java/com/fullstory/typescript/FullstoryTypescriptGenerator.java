69703a4c901b69c06b5baea65e4612a9037b4c0a
Sabrina <sabrina@fullstory.com>
2023-02-27 16:42:48 -0500

    update generator to create models inside sub folders

--- openapi-generator/src/main/java/com/fullstory/typescript/FullstoryTypescriptGenerator.java
+++ openapi-generator/src/main/java/com/fullstory/typescript/FullstoryTypescriptGenerator.java
@@ -11,7 +11,7 @@ public class FullstoryTypescriptGenerator extends AbstractTypeScriptClientCodege
 
   // source folder where to write the files
   protected String sourceFolder = "src";
-  protected String apiVersion = "1.0.0";
+  protected String fsPrefix = "fullstory.v2";
 
   /**
    * Configures the type of generator.
@@ -65,23 +65,111 @@ public class FullstoryTypescriptGenerator extends AbstractTypeScriptClientCodege
   }
 
   /**
-   * Location to write model files. You can use the modelPackage() as defined when
-   * the class is
-   * instantiated
-   */
-  public String modelFileFolder() {
-    // TODO(sabrina): put into correct folder
-    return outputFolder + "/" + sourceFolder + "/" + modelPackage().replace('.', File.separatorChar);
+   * Override toTypescriptTypeName to strip off the fullstory fqn prefixes, before
+   * calling the super method.
+   * This takes the last segment of the named schema, if starting with the
+   * fullstory prefix, as the Type's Name.
+   **/
+  @Override
+  protected String toTypescriptTypeName(String name, String safePrefix) {
+    if (name != null && name.startsWith(fsPrefix)) {
+      String[] segments = name.split("\\.");
+      if (segments.length > 2) {
+        name = segments[segments.length - 1];
+      }
+    }
+
+    return super.toTypescriptTypeName(name, safePrefix);
   }
 
   /**
-   * Location to write api files. You can use the apiPackage() as defined when the
-   * class is
-   * instantiated
+   * Root location to write all model files.
    */
   @Override
-  public String apiFileFolder() {
-    // TODO(sabrina): put into correct folder
-    return outputFolder + "/" + sourceFolder + "/" + apiPackage().replace('.', File.separatorChar);
+  public String modelFileFolder() {
+    String folder = "";
+    if (outputFolder != "") {
+      folder += outputFolder + File.separator;
+    }
+    if (sourceFolder != "") {
+      folder += sourceFolder + File.separator;
+    }
+    folder += modelPackage.replace('.', File.separatorChar);
+    return folder;
+  }
+
+  /**
+   * Subfolder inside modelFileFolder() to write model files.
+   */
+  private String toModelFolderName(String name) {
+    if (name != null && name.startsWith(fsPrefix)) {
+      String[] segments = name.split("\\.");
+      if (segments.length > 2) {
+        name = segments[2];
+      }
+    }
+    return name;
+  }
+
+  @Override
+  public String modelFilename(String templateName, String modelName) {
+    String suffix = modelTemplateFiles().get(templateName);
+    String filename = modelFileFolder() + File.separator + toModelFolderName(modelName) + File.separator
+        + toModelFilename(modelName) + suffix;
+    return filename;
+  }
+
+  /**
+   * Post-processing for all models to with TS specific fields.
+   */
+  @Override
+  public Map<String, ModelsMap> postProcessAllModels(Map<String, ModelsMap> objs) {
+    Map<String, ModelsMap> models = super.postProcessAllModels(objs);
+
+    // first loop over all models map and create import map for all models
+    Map<String, String> importLocMap = overrideAllImportsPathsInModels(models);
+
+    // loop over all models to add "tsImport" to each model
+    addTsImportsToModels(models, importLocMap);
+
+    return models;
+  }
+
+  private Map<String, String> overrideAllImportsPathsInModels(Map<String, ModelsMap> models) {
+    Map<String, String> importLocMap = new HashMap<>();
+    for (String modelName : models.keySet()) {
+      ModelsMap entry = models.get(modelName);
+      for (ModelMap mo : entry.getModels()) {
+        String className = mo.getModel().getClassname();
+        // TODO(sabrina): handle possible duplicate classnames from different path?
+        String tsImportRelPath = toModelFolderName(modelName) + "/" + className;
+
+        mo.put("importPath", tsImportRelPath);
+        importLocMap.put(className, tsImportRelPath);
+      }
+    }
+    return importLocMap;
+  }
+
+  private void addTsImportsToModels(Map<String, ModelsMap> models, Map<String, String> importLocMap) {
+    for (String modelName : models.keySet()) {
+
+      ModelsMap entry = models.get(modelName);
+      for (ModelMap mo : entry.getModels()) {
+
+        CodegenModel cm = mo.getModel();
+        List<Map<String, String>> tsImports = new ArrayList<>();
+
+        for (String im : cm.imports) {
+          HashMap<String, String> tsImport = new HashMap<>();
+          tsImport.put("classname", im);
+          tsImport.put("filename", importLocMap.get(im));
+          tsImports.add(tsImport);
+        }
+
+        mo.put("tsImports", tsImports);
+      }
+
+    }
   }
 }
