c2a5ac43709f79b242372aaf6a0b66bd547981c0
sabrina-li <sabrinali0312@gmail.com>
2023-03-07 13:43:31 -0500

    Sabrina/gen api classes for `users` apis (#5)

--- openapi-generator/src/main/java/com/fullstory/typescript/FullstoryTypescriptGenerator.java
+++ openapi-generator/src/main/java/com/fullstory/typescript/FullstoryTypescriptGenerator.java
@@ -1,15 +1,20 @@
 package com.fullstory.typescript;
 
+import io.swagger.v3.oas.models.Operation;
+import io.swagger.v3.oas.models.tags.Tag;
 import org.openapitools.codegen.*;
 import org.openapitools.codegen.model.*;
+import org.openapitools.codegen.utils.CamelizeOption;
 import org.openapitools.codegen.languages.AbstractTypeScriptClientCodegen;
 
 import java.util.*;
+import java.util.stream.Collectors;
 import java.io.File;
 
-public class FullstoryTypescriptGenerator extends AbstractTypeScriptClientCodegen {
+import static org.openapitools.codegen.utils.StringUtils.camelize;
 
-  // source folder where to write the files
+public class FullstoryTypescriptGenerator extends AbstractTypeScriptClientCodegen {
+  // source folder where to write the files, relative to root
   protected String sourceFolder = "src";
   protected String fsPrefix = "fullstory.v2";
 
@@ -59,9 +64,16 @@ public class FullstoryTypescriptGenerator extends AbstractTypeScriptClientCodege
     super.processOpts();
 
     modelTemplateFiles.put("model.mustache", ".ts");
+    apiTemplateFiles.put("api-single.mustache", ".ts");
+
     supportingFiles
         .add(new SupportingFile("model-index.mustache",
-            sourceFolder + File.separator + modelPackage().replace('.', File.separatorChar), "index.ts"));
+            getSourceFolder() + File.separator + modelDir(),
+            "index.ts"));
+    supportingFiles
+        .add(new SupportingFile("api-index.mustache",
+            getSourceFolder() + File.separator + apiDir(),
+            "index.ts"));
   }
 
   /**
@@ -82,20 +94,49 @@ public class FullstoryTypescriptGenerator extends AbstractTypeScriptClientCodege
     return super.toTypescriptTypeName(name, safePrefix);
   }
 
+  protected String modelDir() {
+    return modelPackage().replace('.', File.separatorChar);
+  }
+
+  protected String apiDir() {
+    return apiPackage().replace('.', File.separatorChar);
+  }
+
+  protected String getSourceFolder() {
+    return sourceFolder;
+  }
+
+  /**
+   * Root location to for source folder.
+   * Source folder contains both api and model folders.
+   * 
+   * @return absolute path to the source folder.
+   */
+  protected String getAbsoluteSourceFolder() {
+    String folder = "";
+    if (!"".equals(getOutputDir())) {
+      folder += getOutputDir() + File.separator;
+    }
+    if (!"".equals(getSourceFolder())) {
+      folder += getSourceFolder();
+    }
+    return folder;
+  }
+
   /**
    * Root location to write all model files.
    */
   @Override
   public String modelFileFolder() {
-    String folder = "";
-    if (outputFolder != "") {
-      folder += outputFolder + File.separator;
-    }
-    if (sourceFolder != "") {
-      folder += sourceFolder + File.separator;
-    }
-    folder += modelPackage().replace('.', File.separatorChar);
-    return folder;
+    return getAbsoluteSourceFolder() + File.separator + modelDir();
+  }
+
+  /**
+   * Root location to write all API files.
+   */
+  @Override
+  public String apiFileFolder() {
+    return getAbsoluteSourceFolder() + File.separator + apiDir();
   }
 
   /**
@@ -107,15 +148,39 @@ public class FullstoryTypescriptGenerator extends AbstractTypeScriptClientCodege
       if (segments.length > 2) {
         name = segments[2];
       }
+    } else {
+      // if not starting with fsPrefix, then don't put in any subfolder.
+      name = "";
     }
-    return name;
+    return camelize(name, CamelizeOption.LOWERCASE_FIRST_LETTER);
   }
 
   @Override
   public String modelFilename(String templateName, String modelName) {
     String suffix = modelTemplateFiles().get(templateName);
-    String filename = modelFileFolder() + File.separator + toModelFolderName(modelName) + File.separator
+    String filename = (!"".equals(modelFileFolder()) ? modelFileFolder() + File.separator : "")
+        + (!"".equals(toModelFolderName(modelName)) ? toModelFolderName(modelName) + File.separator : "")
         + toModelFilename(modelName) + suffix;
+    return filename;
+  }
+
+  /**
+   * Subfolder inside apiFileFolder() to write api files.
+   */
+  private String toApiFolderName(String tag) {
+    String[] segments = tag.split("\\.");
+    if (segments.length > 0) {
+      tag = segments[0];
+    }
+    return camelize(tag, CamelizeOption.LOWERCASE_FIRST_LETTER);
+  }
+
+  @Override
+  public String apiFilename(String templateName, String tag) {
+    String suffix = apiTemplateFiles().get(templateName);
+    String filename = (!"".equals(apiFileFolder()) ? apiFileFolder() + File.separator : "")
+        + (!"".equals(toApiFolderName(tag)) ? toApiFolderName(tag) + File.separator : "")
+        + toApiFilename(tag) + suffix;
     return filename;
   }
 
@@ -130,9 +195,64 @@ public class FullstoryTypescriptGenerator extends AbstractTypeScriptClientCodege
     Map<String, String> importLocMap = overrideAllImportsPathsInModels(models);
 
     // loop over all models to add "tsImport" to each model
+    // TODO(sarbina): maybe use toModelImport?
     addTsImportsToModels(models, importLocMap);
 
     return models;
+  }
+
+  /**
+   * Post-processing for all api operations to with TS specific fields.
+   */
+  @Override
+  public OperationsMap postProcessOperationsWithModels(OperationsMap operations, List<ModelMap> allModels) {
+    OperationMap operationMap = operations.getOperations();
+
+    // put ts imports, for now, api files only imports form @model
+    Map<String, String> importPathMap = new HashMap<>();
+    for (ModelMap mo : allModels) {
+      String importPath = setImportPathForModel(mo.getModel().getName(), mo);
+      importPathMap.put(mo.getModel().getClassname(), importPath);
+    }
+    Map<String, String> tsImports = new HashMap<>();
+    for (CodegenOperation op : operationMap.getOperation()) {
+      for (String im : op.imports) {
+        tsImports.put(im, importPathMap.get(im));
+      }
+      // should always have a named return type, unless api does not return anything.
+      // i.e. DELETE requests
+      if (op.returnType == "object") {
+        op.returnType = null;
+      }
+    }
+    operations.put("tsImports", tsImports);
+    // TODO(sarbina): maybe use toApiImport?
+    operationMap.put("importPath", toApiFolderName(operationMap.getPathPrefix()));
+
+    return operations;
+  }
+
+  @Override
+  public void addOperationToGroup(
+      String tag,
+      String resourcePath,
+      Operation operation,
+      CodegenOperation co,
+      Map<String, List<CodegenOperation>> operations) {
+    // Operations are added to OperationsMap based on tags. If more than one tag is
+    // added for an operation, there will be duplicates in the group. Remove any
+    // duplicate operations and only process it once when seeing the first tag.
+    if (co.tags.size() == 0) {
+      super.addOperationToGroup(tag, resourcePath, operation, co, operations);
+      return;
+    }
+
+    super.addOperationToGroup(sanitizeAndJoinTags(".", co.tags), resourcePath, operation, co, operations);
+  }
+
+  private String sanitizeAndJoinTags(String separator, List<Tag> tags) {
+    List<String> cleanTagNames = tags.stream().map(t -> sanitizeTag(t.getName())).collect(Collectors.toList());
+    return String.join(separator, cleanTagNames);
   }
 
   private Map<String, String> overrideAllImportsPathsInModels(Map<String, ModelsMap> models) {
@@ -140,36 +260,44 @@ public class FullstoryTypescriptGenerator extends AbstractTypeScriptClientCodege
     for (String modelName : models.keySet()) {
       ModelsMap entry = models.get(modelName);
       for (ModelMap mo : entry.getModels()) {
-        String className = mo.getModel().getClassname();
-        // TODO(sabrina): handle possible duplicate classnames from different path?
-        String tsImportRelPath = toModelFolderName(modelName) + "/" + className;
 
-        mo.put("importPath", tsImportRelPath);
+        String className = mo.getModel().getClassname();
+        String tsImportRelPath = setImportPathForModel(modelName, mo);
         importLocMap.put(className, tsImportRelPath);
       }
     }
     return importLocMap;
   }
 
+  private String setImportPathForModel(String modelName, ModelMap mo) {
+    String className = mo.getModel().getClassname();
+    // TODO(sabrina): handle possible duplicate classnames from different path?
+    String tsImportRelPath = toModelFolderName(modelName) + "/" + className;
+
+    mo.put("importPath", tsImportRelPath);
+    return tsImportRelPath;
+  }
+
   private void addTsImportsToModels(Map<String, ModelsMap> models, Map<String, String> importLocMap) {
     for (String modelName : models.keySet()) {
-
       ModelsMap entry = models.get(modelName);
       for (ModelMap mo : entry.getModels()) {
-
-        CodegenModel cm = mo.getModel();
-        List<Map<String, String>> tsImports = new ArrayList<>();
-
-        for (String im : cm.imports) {
-          HashMap<String, String> tsImport = new HashMap<>();
-          tsImport.put("classname", im);
-          tsImport.put("filename", importLocMap.get(im));
-          tsImports.add(tsImport);
-        }
-
-        mo.put("tsImports", tsImports);
+        addTsImportsToModel(mo, importLocMap);
       }
-
     }
+  }
+
+  private void addTsImportsToModel(ModelMap mo, Map<String, String> importLocMap) {
+    CodegenModel cm = mo.getModel();
+    List<Map<String, String>> tsImports = new ArrayList<>();
+
+    for (String im : cm.imports) {
+      HashMap<String, String> tsImport = new HashMap<>();
+      tsImport.put("classname", im);
+      tsImport.put("filename", importLocMap.get(im));
+      tsImports.add(tsImport);
+    }
+
+    mo.put("tsImports", tsImports);
   }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,9 @@
-/**
- * RequestOptions provide additional options to be applied when calling FullStory APIs.
-*/
-export interface RequestOptions {
-    /** An optional integration source string denoting the origin of the requests. */
-    readonly integration_src?: string;
-}
-
-/**
- * FullStoryOptions provide additional options for instantiating the FullStory Client.
-*/
-export interface FullStoryOptions extends RequestOptions {
-    readonly apiKey: string;
-}
+import { UsersApi, UsersBatchImportApi } from './api';
+import { FullStoryOptions } from './http/options';
 
 // TODO(sabrina): create the interface
 export interface FullStoryClient {
+    users: UsersApi & UsersBatchImportApi;
 }
 
 // TODO(sabrina): create the implementation
@@ -23,5 +12,4 @@ class FullStoryImpl implements FullStoryClient {
 }
 
 export function init(opts: FullStoryOptions): FullStoryClient {
-    return new FullStoryImpl(opts);
 }
