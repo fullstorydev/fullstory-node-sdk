e739b4a00278c68b27c0c912a621ca5cb46ebdae
Sabrina <sabrina@fullstory.com>
2023-03-23 16:18:34 -0400

    better import syntax

--- src/model/index.ts
+++ src/model/index.ts
@@ -1,10 +1,7 @@
-import * as events from './events.index';
-import * as users from './users.index';
+/* eslint-disable import/export */
 
 // TODO(sabrina): unfortunately since individual api spec all needs to be generated
 // separately, shared models (such as JobMetadata) gets exported in both files.
 // Here simply take the first one here.
-export default {
-    ...users,
-    ...events
-};
+export * from './events.index';
+export * from './users.index';
