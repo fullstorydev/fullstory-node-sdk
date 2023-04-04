a8ae5271dcba49e303c6a84f1a5d71c28845148b
Sabrina <sabrina@fullstory.com>
2023-04-04 13:59:48 -0400

    add the events api to public fullstory interface

--- src/fullstory.ts
+++ src/fullstory.ts
@@ -1,14 +1,18 @@
+import { Events, IBatchEventsApi, IEventsApi } from './events';
 import { FullStoryOptions } from './http';
 import { IBatchUsersApi, IUsersApi, Users } from './users';
 
 export interface FullStoryClient {
-    users: IBatchUsersApi & IUsersApi;
+    readonly users: IBatchUsersApi & IUsersApi;
+    readonly events: IBatchEventsApi & IEventsApi;
 }
 
 export class FullStoryImpl implements FullStoryClient {
-    users: IBatchUsersApi & IUsersApi;
+    readonly users: IBatchUsersApi & IUsersApi;
+    readonly events: IBatchEventsApi & IEventsApi;
 
     constructor(private opts: FullStoryOptions) {
         this.users = new Users(opts);
+        this.events = new Events(opts);
     }
 }
