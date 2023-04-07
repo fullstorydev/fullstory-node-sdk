6c301a6e7320ee61c3da575cc400f5d1dd4a0a41
Sabrina <sabrina@fullstory.com>
2023-04-07 16:52:24 -0400

    typed test

--- src/batch/__test__/index.test.ts
+++ src/batch/__test__/index.test.ts
@@ -1,9 +1,10 @@
 import { beforeEach, describe, expect, jest, test } from '@jest/globals';
 import { JobStatus } from '@model/events.index';
+import { BatchUserImportRequest, BatchUserImportResponse, CreateUserResponse, FailedUserImport, GetBatchUserImportStatusResponse } from '@model/users.index';
 
 import { BatchJob, IBatchRequester } from '..';
 
-const mockRequester: jest.Mocked<IBatchRequester<any, any, any, any>> = {
+const mockRequester: jest.Mocked<IBatchRequester<GetBatchUserImportStatusResponse, BatchUserImportRequest, BatchUserImportResponse, FailedUserImport>> = {
     requestCreateJob: jest.fn(),
     requestImports: jest.fn(),
     requestImportErrors: jest.fn(),
@@ -14,17 +15,18 @@ beforeEach(() => {
     jest.resetAllMocks();
 });
 
-describe('FullStory Events API', () => {
+describe('BatchJob', () => {
     test('more requests can be added', () => {
-        const baseJob = new BatchJob<'users', any, any, any, any>([], mockRequester, {});
-        expect(baseJob.requests).toHaveLength(0);
-        baseJob.add([1, 2, 3]);
+        const req: BatchUserImportRequest = { display_name: 'test batch job' };
+        const baseJob = new BatchJob([req], mockRequester, {});
+        expect(baseJob.requests).toHaveLength(1);
+        baseJob.add([req, req]);
         expect(baseJob.requests).toHaveLength(3);
     });
 
     test('can execute', () => {
         mockRequester.requestCreateJob = jest.fn(async _ => { return { id: 'test' }; });
-        const baseJob = new BatchJob<'users', any, any, any, any>([], mockRequester, {});
+        const baseJob = new BatchJob([], mockRequester, {});
         baseJob.execute();
         expect(mockRequester.requestCreateJob).toHaveBeenCalledTimes(1);
     });
@@ -33,7 +35,7 @@ describe('FullStory Events API', () => {
         mockRequester.requestCreateJob = jest.fn(async _ => { return { id: 'test' }; });
         mockRequester.requestJobStatus = jest.fn(async _ => { return { job: { status: JobStatus.Processing } }; });
 
-        const baseJob = new BatchJob<'users', any, any, any, any>([], mockRequester, {});
+        const baseJob = new BatchJob([], mockRequester, {});
         baseJob.on('processing', _ => {
             done();
         });
@@ -44,7 +46,7 @@ describe('FullStory Events API', () => {
         mockRequester.requestCreateJob = jest.fn(async _ => { return { id: 'test' }; });
         mockRequester.requestJobStatus = jest.fn(async _ => { throw new Error('test'); });
 
-        const baseJob = new BatchJob<'users', any, any, any, any>([], mockRequester, {});
+        const baseJob = new BatchJob([], mockRequester, {});
         baseJob.on('error', _ => {
             done();
         });
@@ -52,26 +54,30 @@ describe('FullStory Events API', () => {
     });
 
     test('on done should be called on job complete', (done) => {
+        const rsp: CreateUserResponse = {};
         mockRequester.requestCreateJob = jest.fn(async _ => { return { id: 'test' }; });
         mockRequester.requestJobStatus = jest.fn(async _ => { return { job: { status: JobStatus.Completed } }; });
-        mockRequester.requestImports = jest.fn(async _ => { return [1, 2, 3]; });
+        mockRequester.requestImports = jest.fn(async _ => { return [rsp, rsp, rsp]; });
 
-        const baseJob = new BatchJob<'users', any, any, any, any>([], mockRequester, {});
+        const baseJob = new BatchJob([], mockRequester, {});
         baseJob.on('done', (i, f) => {
-            console.log(i, f);
+            expect(i).toHaveLength(3);
+            expect(f).toHaveLength(0);
             done();
         });
         baseJob.execute();
     });
 
     test('on done should be called on job complete with failure', (done) => {
+        const rsp: FailedUserImport = {};
         mockRequester.requestCreateJob = jest.fn(async _ => { return { id: 'test' }; });
         mockRequester.requestJobStatus = jest.fn(async _ => { return { job: { status: JobStatus.Failed } }; });
-        mockRequester.requestImportErrors = jest.fn(async _ => { return [1, 2, 3]; });
+        mockRequester.requestImportErrors = jest.fn(async _ => { return [rsp, rsp, rsp]; });
 
         const baseJob = new BatchJob<'users', any, any, any, any>([], mockRequester, {});
         baseJob.on('done', (i, f) => {
-            console.log(i, f);
+            expect(i).toHaveLength(0);
+            expect(f).toHaveLength(3);
             done();
         });
         baseJob.execute();
