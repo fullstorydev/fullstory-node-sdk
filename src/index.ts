423e084dabca2f1988e830da597e0cb0b1406f65
Sabrina <sabrina@fullstory.com>
2023-04-18 13:07:21 -0400

    prefix api key with 'basic'

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,13 @@
+import { FSUnknownError } from './errors';
 import { FullStoryClient, FullStoryImpl } from './fullstory';
 import { FullStoryOptions } from './http';
 
 export function init(opts: FullStoryOptions): FullStoryClient {
-    return new FullStoryImpl(opts);
+    if (!opts.apiKey) {
+        throw new FSUnknownError('apiKey is required in opts');
+    }
+    const apiKey = opts.apiKey.indexOf(' ') < 0 ? `Basic ${opts.apiKey}` : opts.apiKey;
+    return new FullStoryImpl({ ...opts, apiKey });
 }
 
 export * from '@api/index';
