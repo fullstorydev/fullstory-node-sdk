eca664067a3643bdd764d8c060ff49a98b52ef5e
sabrina-li <sabrinali0312@gmail.com>
2023-06-23 15:01:48 -0400

    Sabrina/fix mock req for testing hosts (#43)

--- src/api/__tests__/util.ts
+++ src/api/__tests__/util.ts
@@ -1,11 +1,20 @@
+import * as dotenv from 'dotenv';
+dotenv.config();
+
 export const defaultHost = 'api.fullstory.com';
 
 export function makeMockReq(basePath: string, method: string, path: string, headers: any = {}) {
+    let host = defaultHost;
+    if (process.env.FS_API_HOST) {
+        const url = new URL(process.env.FS_API_HOST);
+        host = url.host;
+    }
+
     return {
         headers: headers,
         protocol: 'https:',
-        hostname: defaultHost,
-        host: defaultHost,
+        hostname: host,
+        host: host,
         port: '',
         method: method,
         path: basePath + path
