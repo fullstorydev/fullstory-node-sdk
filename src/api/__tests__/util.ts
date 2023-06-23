e57a7bac5203e83e155246b48450b45f36ae7a32
Sabrina <sabrina@fullstory.com>
2023-06-23 14:30:37 -0400

    make mock request use .env

--- src/api/__tests__/util.ts
+++ src/api/__tests__/util.ts
@@ -1,7 +1,15 @@
+import * as dotenv from 'dotenv';
+dotenv.config();
+
 export const defaultHost = 'api.fullstory.com';
 
 export function makeMockReq(basePath: string, method: string, path: string, headers: any = {}) {
-    const host = process.env.FS_API_HOST || defaultHost;
+    let host = defaultHost;
+    if (process.env.FS_API_HOST) {
+        const url = new URL(process.env.FS_API_HOST);
+        host = url.host;
+    }
+
     return {
         headers: headers,
         protocol: 'https:',
