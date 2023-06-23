15379a2e0fbfe615b6dcc2c9a05908675dc5bedd
Sabrina <sabrina@fullstory.com>
2023-06-23 14:20:18 -0400

    fix mock request

--- src/api/__tests__/util.ts
+++ src/api/__tests__/util.ts
@@ -1,11 +1,12 @@
 export const defaultHost = 'api.fullstory.com';
 
 export function makeMockReq(basePath: string, method: string, path: string, headers: any = {}) {
+    const host = process.env.FS_API_HOST || defaultHost;
     return {
         headers: headers,
         protocol: 'https:',
-        hostname: defaultHost,
-        host: defaultHost,
+        hostname: host,
+        host: host,
         port: '',
         method: method,
         path: basePath + path
