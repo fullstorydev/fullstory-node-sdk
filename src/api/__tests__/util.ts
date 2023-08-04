caf8eeca88a9f02e02a2ce1ccc957405193af947
Sabrina <sabrina@fullstory.com>
2023-08-04 10:48:52 -0400

    fix mock if using custom port (for localdev)

--- src/api/__tests__/util.ts
+++ src/api/__tests__/util.ts
@@ -5,17 +5,23 @@ export const defaultHost = 'api.fullstory.com';
 
 export function makeMockReq(basePath: string, method: string, path: string, headers: any = {}) {
     let host = defaultHost;
+    let hostname = defaultHost;
+    let port = '';
+    let protocol = 'https:';
     if (process.env.FS_API_HOST) {
         const url = new URL(process.env.FS_API_HOST);
         host = url.host;
+        hostname = url.hostname;
+        port = url.port;
+        protocol = url.protocol;
     }
 
     return {
         headers: headers,
-        protocol: 'https:',
-        hostname: host,
+        protocol: protocol,
+        hostname: hostname,
         host: host,
-        port: '',
+        port: port,
         method: method,
         path: basePath + path
     };
