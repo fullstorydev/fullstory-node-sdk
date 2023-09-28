9a870f6b6511c34a3bc55f601fca74c619d09602
Sabrina <sabrina@fullstory.com>
2023-09-28 10:59:05 -0400

    add comment for integration source

--- src/http/options.ts
+++ src/http/options.ts
@@ -2,7 +2,11 @@
  * RequestOptions provide additional options to be applied when calling FullStory APIs.
  */
 export interface FSRequestOptions {
-    /** An optional Integration-Source string denoting the origin of the requests. */
+    /** An optional Integration-Source string denoting the origin of the requests.
+     * the integrationSource is generally intended for FullStory developers
+     * or FullStory partners while building integrations.
+     * Generally it should be left empty.
+    */
     readonly integrationSource?: string;
 }
 
